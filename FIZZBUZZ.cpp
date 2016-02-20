4c74822d8c76a7a0ec0d67d6c1f07eb76b89960a
Akshay2010 <214504441@stu.ukzn.ac.za>
2016-02-20 16:41:49 +0200

    FIZZBUZZ

--- FIZZBUZZ.cpp
+++ FIZZBUZZ.cpp
@@ -1,41 +1,24 @@
-#include<iostream>
 #include<stdio.h>
+#include<iostream>
 
 using namespace std;
 
-
 int main()
 {
+
+	
 	int i=1;
-	int status=0;
+	bool status=0;
 
-for(i=1;i<=100;i++)
+	for(;i<=20;i++)
 	{
-			if((i%3 == 0) && ((i%5)==0) && ((i%7)==0))
-			{
-				cout<<"FizzBuzzWoof"<<endl;
-				status=1;
-			}
+		if((i%3 == 0) && ((i%5)==0))
+		{
+			cout<<"Fizzbuzz"<<endl;
+			bool status =1;
+		}
 
-			if(((i%3 == 0) && ((i%5)==0))&& status==0)
-			{
-				cout<<"Fizzbuzz"<<endl;
-				status=1;
-			}
-
-			if(((i%3 == 0) && ((i%7)==0))&& status==0)
-			{
-				cout<<"FizzWoof"<<endl;
-				status=1;
-			}
-
-			if(((i%3 == 0) && ((i%5)==0))&& status==0)
-			{
-				cout<<"buzzwoof"<<endl;
-				status=1;
-			}
-
-			if((i%3 == 0) && status==0)
+		if((i%3 == 0) && status==0)
 		{
 			cout<<"Fizz"<<endl;
 			status=1;
@@ -47,21 +30,14 @@ for(i=1;i<=100;i++)
 			status=1;
 		};
 
-		if((i%7 == 0) && status==0)
-		{
-			cout<<"woof"<<endl;
-			status=1;
-		};
-
-
 		if(status==0)
 			cout<<i<<endl;
-			
+		
+		
 		status=0;
 	}
 
 	system("PAUSE");
-
-return 0;
+	return 0;
 
 }
