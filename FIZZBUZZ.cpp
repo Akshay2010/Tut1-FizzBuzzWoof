48fe5a0a5e86d9bfba9c847a86e737f0174a2c8e
Akshay2010 <214504441@stu.ukzn.ac.za>
2016-02-20 16:30:45 +0200

    FIZZBUZZ

---
+++ FIZZBUZZ.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+
+int main()
+{
+	int i=1;
+	int status=0;
+
+for(;i<=20;i++)
+	{
+		if((i%3 == 0) && ((i%5)==0))
+		{
+			cout<<"Fizzbuzz"<<endl;
+			bool status =1;
+		}
+
+		if((i%3 == 0) && status==0)
+		{
+			cout<<"Fizz"<<endl;
+			status=1;
+		}
+
+		if((i%5 == 0) && status==0)
+		{
+			cout<<"buzz"<<endl;
+			status=1;
+		};
+
+		if(status==0)
+			cout<<i<<endl;
+		
+		
+		status=0;
+	}
+
+	system("PAUSE");
+
+return 0;
+
+}
